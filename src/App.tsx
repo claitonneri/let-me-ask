d4bfa99cbf31357aba4361344a37831d5bdab156
Claiton Neri <claitondidone@gmail.com>
2021-11-30 00:00:14 -0300

    chore: add global theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
+import { ThemeProvider } from 'styled-components';
+
 import Home from "./pages/Home";
 
+import theme from './styles/theme';
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Home />
       <GlobalStyle />
-      </>
+    </ThemeProvider>
   );
 }
 
