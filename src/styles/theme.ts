01881e487a487063a30dd96f41cbaaf1be047249
Claiton Neri <claitondidone@gmail.com>
2021-12-01 23:14:21 -0300

    style: add style options

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,9 @@
 export default {
   colors: {
-    primary: '#835afd',
+    primary: '#2d2d2c',
 
-    text: '#f8f8f8'
+    text: '#f8f8f8',
+    description: '#737380',
+    background: '#a8a8b3'
   }
 }
